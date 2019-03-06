7b37823c75b348adeb9ba825644f358d05c5f624
zepod <albert.nemec2457@gmail.com>
2019-03-06 22:32:24 +0100

    Add modules

--- js/uncreative.js
+++ js/uncreative.js
@@ -15,3 +15,24 @@ onAnchorClick((event) => {
   const distanceToSection = requestedSection.offsetTop - 56;
   bodyElement.scrollTop = distanceToSection;
 });
+
+const yourplaceModals = ['module-obyvak', 'module-lekce', 'module-off', 'module-senior', 'module-events', 'module-road'];
+
+
+document.addEventListener('keydown', (event) => {
+  if (event.keyCode === 27) {
+    window.hideYourPlaceModals();
+  }
+});
+
+window.hideYourPlaceModals = () => {
+  yourplaceModals.forEach((selector) => {
+    document.querySelector(`#${selector}`).style.display = 'none';
+    document.querySelector('#gay-overlay').style.display = 'none';
+  });
+};
+
+window.showYourPlaceModal = (selector) => {
+  document.querySelector(`#${selector}`).style.display = 'block';
+  document.querySelector('#gay-overlay').style.display = 'block';
+};
